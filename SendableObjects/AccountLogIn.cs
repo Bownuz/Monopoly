ea1e9e01e8363f623e656c439b442758984623e2
tiemenbr <tiemenbr@hotmail.nl>
2024-10-25 20:42:39 +0200

    added hashing for messages and passwords

--- SendableObjects/AccountLogIn.cs
+++ SendableObjects/AccountLogIn.cs
@@ -1,5 +1,16 @@
-﻿namespace SendableObjects {
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SendableObjects {
     [Serializable]
     public class AccountLogIn {
+        string name { get; }
+        byte[] passwdHash { get; }
+
+        public AccountLogIn(string name, string passwd) {
+            this.name = name;
+            this.passwdHash = SHA512.HashData(Encoding.UTF8.GetBytes(passwd));
+        }
     }
 }
